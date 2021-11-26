90515b1b14daa082c1a26fe6de5f08300e649659
vishnuacappella <acappella.studiov@gmail.com>
2021-11-26 10:29:02 +0530

    ini it

---
+++ lib/home.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+  String name = 'Finity';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SizedBox(
+          // width: double.infinity,
+          // height: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                flex: 2,
+                child: Container(
+                  color: Colors.amberAccent,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        name,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          color: Colors.red,
+                          fontSize: 50,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          TextButton(
+                            onPressed: () {
+                              // ignore: avoid_print
+                              print('Text Butoon clicked');
+                            },
+                            child: const Text('Click Me'),
+                          ),
+                          IconButton(
+                              onPressed: () {}, icon: const Icon(Icons.mic))
+                        ],
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          // ignore: avoid_print
+                          print('Elevated Butoon clicked');
+                        },
+                        child: const Text('Click Me'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.limeAccent,
+                    borderRadius: BorderRadius.circular(50),
+                    border: Border.all(
+                      color: Colors.green,
+                      width: 5,
+                    ),
+                  ),
+                  child: const Center(
+                    child: Text('Text'),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.greenAccent,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        name,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          color: Colors.red,
+                          fontSize: 50,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          TextButton(
+                            onPressed: () {
+                              // ignore: avoid_print
+                              print('Text Butoon clicked');
+                            },
+                            child: const Text('Click Me'),
+                          ),
+                          IconButton(
+                              onPressed: () {}, icon: const Icon(Icons.mic))
+                        ],
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          // ignore: avoid_print
+                          print('Elevated Butoon clicked');
+                        },
+                        child: const Text('Click Me'),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
